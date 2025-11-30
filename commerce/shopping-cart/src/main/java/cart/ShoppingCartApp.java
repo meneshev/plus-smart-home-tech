a613996dc5e248a3ad36baa7107ac11a5b6b47fe
Tim Meneshev <tim130512@ya.ru>
2025-11-30 17:11:20 +0700

    start final task

---
+++ commerce/shopping-cart/src/main/java/cart/ShoppingCartApp.java
@@ -0,0 +1,11 @@
+package cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartApp.class, args);
+    }
+}
---
+++ commerce/warehouse/src/main/java/warehouse/WarehouseApp.java
@@ -0,0 +1,11 @@
+package warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApp.class, args);
+    }
+}
